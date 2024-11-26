1655b1845eb34fe4d6f805e9f101008ebe587e7e
Arvin Buid <arvinbuid01@gmail.com>
2024-11-26 19:13:52 +0800

    Create schema and migration into the database.

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,7 +1,12 @@
-export default function Page() {
+import prisma from '@/prisma/db';
+
+export default async function Page() {
+  const tickets = await prisma.ticket.findMany();
+
+  console.log(tickets);
   return (
     <div>
-      <h1>Ticketing App</h1>
+      <h1>Tickets</h1>
     </div>
   );
 }
