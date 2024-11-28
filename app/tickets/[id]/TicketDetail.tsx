449dada7f9e3488e9bf08693f79927de43059a6a
Arvin Buid <arvinbuid01@gmail.com>
2024-11-28 23:26:00 +0800

    Add styles in TicketDetail component using the installed Card component.

--- app/tickets/[id]/TicketDetail.tsx
+++ app/tickets/[id]/TicketDetail.tsx
@@ -1,4 +1,16 @@
 import { Ticket } from '@prisma/client';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import TicketStatusBadge from '@/components/TicketStatusBadge';
+import TicketPriority from '@/components/TicketPriority';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
 
 interface TicketDetailProps {
   ticket: Ticket;
@@ -6,9 +18,54 @@ interface TicketDetailProps {
 
 export default function TicketDetail({ ticket }: TicketDetailProps) {
   return (
-    <div>
-      <p>{ticket.title}</p>
-      <p>{ticket.description}</p>
+    <div className="lg:grid lg:grid-cols-4">
+      <Card className="mr-4 mb-4 lg:col-span-3">
+        <CardHeader>
+          <div className="flex justify-between mb-4">
+            <TicketStatusBadge status={ticket.status} />
+            <TicketPriority priority={ticket.priority} />
+          </div>
+
+          <CardTitle>{ticket.title}</CardTitle>
+          <CardDescription>
+            Created At:{' '}
+            {ticket.createdAt.toLocaleDateString('en-PH', {
+              year: '2-digit',
+              month: '2-digit',
+              day: '2-digit',
+              hour: 'numeric',
+              minute: '2-digit',
+              hour12: true,
+            })}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>{ticket.description}</CardContent>
+        <CardFooter>
+          Updated At:{' '}
+          {ticket.updatedAt.toLocaleDateString('en-PH', {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit',
+            hour: 'numeric',
+            minute: '2-digit',
+            hour12: true,
+          })}
+        </CardFooter>
+      </Card>
+      <div className="flex lg:flex-col gap-2">
+        <Link
+          href={`tickets/edit/${ticket.id}`}
+          className={buttonVariants({ variant: 'default' })}
+        >
+          Edit Ticket
+        </Link>
+        <Link
+          href={`tickets/delete/${ticket.id}`}
+          className={buttonVariants({ variant: 'default' })}
+        >
+          Delete Ticket
+        </Link>
+      </div>
     </div>
   );
 }
--- components/TicketPriority.tsx
+++ components/TicketPriority.tsx
@@ -13,7 +13,7 @@ const priorityMap: Record<Priority, { label: string; level: 1 | 2 | 3 }> = {
 
 export default function TicketPriority({ priority }: TicketPriorityProps) {
   return (
-    <>
+    <div className="flex">
       <Flame
         className={`${
           priorityMap[priority].level >= 1 ? 'text-red-500' : 'text-muted'
@@ -29,6 +29,6 @@ export default function TicketPriority({ priority }: TicketPriorityProps) {
           priorityMap[priority].level >= 3 ? 'text-red-500' : 'text-muted'
         }`}
       />
-    </>
+    </div>
   );
 }
