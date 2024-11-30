686115275cad030308582743c93fd7e06f9f4258
Arvin Buid <arvinbuid01@gmail.com>
2024-11-30 19:05:55 +0800

    Implement dynamic text inside MainNav to display login or logout depending if user is logged in or not.

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,14 +1,23 @@
 import Link from 'next/link';
 import ToggleMode from './ToggleMode';
 import MainNavLinks from './MainNavLinks';
+import { getServerSession } from 'next-auth';
+import options from '@/app/api/auth/[...nextauth]/options';
 
-export default function MainNav() {
+export default async function MainNav() {
+  const session = await getServerSession(options);
+
   return (
     <section className="flex justify-between">
       <MainNavLinks />
 
       <div className="flex items-center gap-2">
-        <Link href="/">Logout</Link>
+        {session ? (
+          <Link href="/api/auth/signout?callback=/">Logout</Link>
+        ) : (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
+
         <ToggleMode />
       </div>
     </section>
