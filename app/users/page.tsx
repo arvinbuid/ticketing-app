ced1025fddaeead8ca5e18468958588c5f3e1c82
Arvin Buid <arvinbuid01@gmail.com>
2024-12-01 14:45:03 +0800

    Enable creating user for deployment.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,19 +1,19 @@
 import UserForm from '@/components/UserForm';
 import DataTable from './DataTable';
 import prisma from '@/prisma/db';
-import { getServerSession } from 'next-auth';
-import options from '../api/auth/[...nextauth]/options';
+// import { getServerSession } from 'next-auth';
+// import options from '../api/auth/[...nextauth]/options';
 
 export default async function Users() {
-  const session = await getServerSession(options);
+  // const session = await getServerSession(options);
 
-  if (session?.user.role !== 'ADMIN') {
-    return (
-      <p className="text-destructive">
-        You must be logged in to access this page.
-      </p>
-    );
-  }
+  // if (session?.user.role !== 'ADMIN') {
+  //   return (
+  //     <p className="text-destructive">
+  //       You must be logged in to access this page.
+  //     </p>
+  //   );
+  // }
 
   const users = await prisma.user.findMany();
   return (
