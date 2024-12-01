77d60d395ad1bab77bde3dc127c870b4cb2eefe0
Arvin Buid <arvinbuid01@gmail.com>
2024-12-01 15:44:45 +0800

    gitignore configuration change.

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,27 +2,27 @@ import { userSchema } from '@/ValidationSchemas/users';
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/db';
 import bcrypt from 'bcryptjs';
-// import { getServerSession } from 'next-auth';
-// import options from '../auth/[...nextauth]/options';
+import { getServerSession } from 'next-auth';
+import options from '../auth/[...nextauth]/options';
 
 export async function POST(request: NextRequest) {
-  // const session = await getServerSession(options);
+  const session = await getServerSession(options);
 
-  // if (!session) {
-  //   return NextResponse.json(
-  //     { error: 'Unauthorized action. Please login to continue' },
-  //     { status: 401 }
-  //   );
-  // }
+  if (!session) {
+    return NextResponse.json(
+      { error: 'Unauthorized action. Please login to continue' },
+      { status: 401 }
+    );
+  }
 
-  // if (session.user.role !== 'ADMIN') {
-  //   return NextResponse.json(
-  //     {
-  //       error: 'Invalid privileges. Only Admin can create a new user!',
-  //     },
-  //     { status: 401 }
-  //   );
-  // }
+  if (session.user.role !== 'ADMIN') {
+    return NextResponse.json(
+      {
+        error: 'Invalid privileges. Only Admin can create a new user!',
+      },
+      { status: 401 }
+    );
+  }
 
   const body = await request.json();
   const validation = userSchema.safeParse(body);
